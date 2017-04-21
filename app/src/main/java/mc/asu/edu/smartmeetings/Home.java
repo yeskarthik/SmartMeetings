c7297c266c7648125a19556dcfebc2013d497cb9
Sairaj Madhavan <sairaj.madhavan@asu.edu>
2017-04-21 03:18:29 -0700

    Location services in background and display weather on homepage

--- app/src/main/java/mc/asu/edu/smartmeetings/Home.java
+++ app/src/main/java/mc/asu/edu/smartmeetings/Home.java
@@ -7,19 +7,36 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Result;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.vision.text.Text;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.RemoteMessage;
 import com.pusher.android.PusherAndroid;
@@ -28,10 +45,19 @@ import com.pusher.android.notifications.fcm.FCMPushNotificationReceivedListener;
 import com.pusher.android.notifications.interests.InterestSubscriptionChangeListener;
 import com.pusher.android.notifications.tokens.PushNotificationRegistrationListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Home extends AppCompatActivity {
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class Home extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     public static final int ASK_MULTIPLE_PERMISSION_REQUEST_CODE =1;
     gpsService service;
@@ -40,6 +66,12 @@ public class Home extends AppCompatActivity {
     String username;
     int mId = 5;
     Context context;
+    private PendingIntent pendingIntent;
+    LocationRequest mLocationRequest;
+    GoogleApiClient mGoogleApiClient;
+    String[] temp = new String[2];
+    Location loc;
+    String weather;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,7 +83,7 @@ public class Home extends AppCompatActivity {
         context = this.getApplicationContext();
         this.username = name;
         System.out.println("username " + name);
-
+        weather = "";
         int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR);
         int permissionCheck1 = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR);
         int permissionCheck2 = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
@@ -69,11 +101,15 @@ public class Home extends AppCompatActivity {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            service = new gpsService(this, findViewById(R.id.weather_box));
-            service.startService();
+            //Intent intent = new Intent(Intent.ACTION_SYNC, null, this, gpsLocation.class);
+            //startService(intent);
+
+            buildGoogleApiClient();
+            System.out.println("Connecting google api");
+            mGoogleApiClient.connect();
+            setTemperature();
 
         }
-
 
         TextView view = (TextView) findViewById(R.id.username);
         view.append("Welcome, "+name);
@@ -111,11 +147,11 @@ public class Home extends AppCompatActivity {
                     // contacts-related task you need to do.
                     System.out.println("Permission granted da thambi");
 
-                    //service = new gpsService(this, findViewById(R.id.weather_box));
-                    //service.startService();
-
-                    Intent intent = new Intent(Intent.ACTION_SYNC, null, this, gpsLocation.class);
-                    startService(intent);
+                    buildGoogleApiClient();
+                    mGoogleApiClient.connect();
+                    setTemperature();
+                    //Intent intent = new Intent(Intent.ACTION_SYNC, null, this, gpsLocation.class);
+                    //startService(intent);
                     try {
                         startPolling();
                     } catch (Exception e) {
@@ -135,6 +171,53 @@ public class Home extends AppCompatActivity {
 
             // other 'case' lines to check for other
             // permissions this app might request
+        }
+    }
+    protected synchronized void buildGoogleApiClient() {
+        //Toast.makeText(,"buildGoogleApiClient",Toast.LENGTH_SHORT).show();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+    public void setTemperature()
+    {
+        System.out.println("I got called");
+        final TextView view = (TextView) findViewById(R.id.weather_box);
+        LocationManager mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        loc= null;
+        try
+        {
+            System.out.println("I am getting location now");
+            loc=mlocManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if(loc == null)
+            {
+                Criteria criteria = new Criteria();
+                String bestprovider = String.valueOf(mlocManager.getBestProvider(criteria, true)).toString();
+                mlocManager.requestLocationUpdates(bestprovider, 1000, 0, this);
+            }
+
+        }
+        catch(SecurityException e)
+        {
+            System.out.println(e);
+        }
+        try
+        {
+            if(loc != null)
+            {
+                locationM locate = new locationM();
+                locate.execute(loc);
+                System.out.println("Weather and temperature" +" "+ loc.getLatitude() +" " + weather);
+                //view.setText("Weather : "+weather);
+            }
+
+
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
         }
     }
 
@@ -182,18 +265,23 @@ public class Home extends AppCompatActivity {
                 @Override
                 public void onMessageReceived(RemoteMessage remoteMessage) {
                     // do something magical
-                    System.out.println("Received from server "+ remoteMessage.getNotification().getBody() + " "+remoteMessage.getNotification().getTitle());
-                    Map<String, String> data = remoteMessage.getData();
-                    Intent submitPoll = new Intent(context, SubmitPoll.class);
-                    Bundle extras = new Bundle();
-                    extras.putString("question", data.get("question"));
-                    extras.putString("option1", data.get("option1"));
-                    extras.putString("option2", data.get("option2"));
-                    extras.putString("option3", data.get("option3"));
-                    extras.putString("option4", data.get("option4"));
-                    extras.putString("id", data.get("poll_id"));
-                    submitPoll.putExtras(extras);
-                    startActivity(submitPoll);
+                    if(remoteMessage != null)
+                    {
+                        System.out.println("Received from server "+ remoteMessage.getNotification().getBody() + " "+remoteMessage.getNotification().getTitle());
+                        Map<String, String> data = remoteMessage.getData();
+                        Intent submitPoll = new Intent(context, SubmitPoll.class);
+                        Bundle extras = new Bundle();
+                        extras.putString("question", data.get("question"));
+                        extras.putString("option1", data.get("option1"));
+                        extras.putString("option2", data.get("option2"));
+                        extras.putString("option3", data.get("option3"));
+                        extras.putString("option4", data.get("option4"));
+                        extras.putString("id", data.get("poll_id"));
+                        submitPoll.putExtras(extras);
+                        startActivity(submitPoll);
+
+                    }
+
                 }
             });
         }
@@ -249,6 +337,118 @@ public class Home extends AppCompatActivity {
         mapIntent.setPackage("com.google.android.apps.maps");
         if (mapIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(mapIntent);
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        System.out.println("connected to google api");
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000*60*30);
+        mLocationRequest.setFastestInterval(10);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        Intent mRequestLocationUpdatesIntent = new Intent(this, gpsLocation.class);
+
+        pendingIntent = PendingIntent.getService(getApplicationContext(), 0,
+                mRequestLocationUpdatesIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        try
+        {
+            PendingResult pendingResult =  LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
+                    mLocationRequest,
+                    pendingIntent);
+
+
+        }
+        catch(SecurityException e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        System.out.println("onConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        System.out.println("Connection failure");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        System.out.println("Location changed ");
+        loc = location;
+        setTemperature();
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    class locationM extends AsyncTask<Location, Void , String>
+    {
+
+        @Override
+        protected String doInBackground(Location... params) {
+
+            String latitude = "" + loc.getLatitude();
+            String longitude = ""+ loc.getLongitude();
+
+            OkHttpClient httpClient = new OkHttpClient();
+            String url="http://api.openweathermap.org/data/2.5/weather?"+"lat="+latitude+"&lon="+longitude+"&appid=a785c4493ca77c844a469d079ef76754";
+            Request request = new Request.Builder().url(url).build();
+            String body="";
+            try
+            {
+                Response response = httpClient.newCall(request).execute();
+                body = response.body().string();
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+            }
+            System.out.println("Received response: "+body);
+
+            String weatherMain = "";
+            String weatherTemp = "";
+            try {
+                JSONObject jsonObject = new JSONObject(body);
+                weatherMain = jsonObject.getJSONArray("weather").getJSONObject(0).getString("main");
+                weatherTemp = jsonObject.getJSONObject("main").getString("temp");
+                temp[0] = weatherMain;
+                temp[1] = weatherTemp;
+                System.out.println("Weather1: " + weatherMain + " " + weatherTemp);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return weatherMain + " " +weatherTemp;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            weather = s;
+            System.out.println("Weather2 "+weather);
+
+            TextView view = (TextView) findViewById(R.id.weather_box);
+            view.setText("Weather: "+weather+"K");
+
         }
     }
 }
